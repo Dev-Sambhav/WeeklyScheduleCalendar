bf6fab79bf6f2e383aed1ef7eab0805beb6910e5
Sambhav Sharma <devSambhav50@gmail.com>
2024-02-01 19:58:04 +0530

    fix warning in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [react()],
+  build: { chunkSizeWarningLimit: 1600 },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  }
-})
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
